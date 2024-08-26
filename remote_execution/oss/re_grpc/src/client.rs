69603c9c5ae742d3250ca927ad1374576dc01ce2
Hugo van der Wijst <hvanderwijst@tesla.com>
2024-08-26 11:37:43 -0700

    Import `dashmap::DashMap`.

--- remote_execution/oss/re_grpc/src/client.rs
+++ remote_execution/oss/re_grpc/src/client.rs
@@ -19,6 +19,7 @@ use std::time::Instant;
 use anyhow::Context;
 use buck2_re_configuration::Buck2OssReConfiguration;
 use buck2_re_configuration::HttpHeader;
+use dashmap::DashMap;
 use dupe::Dupe;
 use futures::future::BoxFuture;
 use futures::future::Future;
@@ -505,7 +506,7 @@ pub struct REClient {
     instance_name: InstanceName,
     // buck2 calls find_missing for same blobs
     find_missing_cache: Mutex<FindMissingCache>,
-    prev_uploads: dashmap::DashMap<TDigest, OngoingUploadStatus>,
+    prev_uploads: DashMap<TDigest, OngoingUploadStatus>,
 }
 
 impl Drop for REClient {
@@ -580,7 +581,7 @@ impl REClient {
                 ttl: Duration::from_secs(12 * 60 * 60), // 12 hours TODO: Tune this parameter
                 last_check: Instant::now(),
             }),
-            prev_uploads: dashmap::DashMap::new(),
+            prev_uploads: DashMap::new(),
         }
     }
 
@@ -1329,7 +1330,7 @@ async fn upload_impl<Byt, Cas>(
     request: UploadRequest,
     max_total_batch_size: usize,
     max_concurrent_uploads: Option<usize>,
-    prev_uploads: &dashmap::DashMap<TDigest, OngoingUploadStatus>,
+    prev_uploads: &DashMap<TDigest, OngoingUploadStatus>,
     cas_f: impl Fn(BatchUpdateBlobsRequest) -> Cas + Sync + Send + Copy,
     bystream_fut: impl Fn(Vec<WriteRequest>) -> Byt + Sync + Send + Copy,
 ) -> anyhow::Result<UploadResponse>
@@ -2265,7 +2266,7 @@ mod tests {
             req,
             10000,
             None,
-            &dashmap::DashMap::new(),
+            &DashMap::new(),
             |req| {
                 let res = res.clone();
                 let digest1 = digest1.clone();
@@ -2349,7 +2350,7 @@ mod tests {
             req,
             10, // kept small to simulate a large file upload
             None,
-            &dashmap::DashMap::new(),
+            &DashMap::new(),
             |req| {
                 let res = res.clone();
                 let digest1 = digest1.clone();
@@ -2424,7 +2425,7 @@ mod tests {
             req,
             10, // kept small to simulate a large inlined upload
             None,
-            &dashmap::DashMap::new(),
+            &DashMap::new(),
             |req| {
                 let res = res.clone();
                 let digest1 = digest1.clone();
@@ -2486,7 +2487,7 @@ mod tests {
             req,
             10,
             None,
-            &dashmap::DashMap::new(),
+            &DashMap::new(),
             |_req| async move {
                 panic!("This should not be called as there are no blobs to upload in batch");
             },
@@ -2548,7 +2549,7 @@ mod tests {
             req,
             3,
             None,
-            &dashmap::DashMap::new(),
+            &DashMap::new(),
             |_req| async move {
                 panic!("Not called");
             },
@@ -2590,7 +2591,7 @@ mod tests {
             req,
             0,
             None,
-            &dashmap::DashMap::new(),
+            &DashMap::new(),
             |_req| async move {
                 panic!("Not called");
             },
@@ -2637,7 +2638,7 @@ mod tests {
             req,
             1,
             None,
-            &dashmap::DashMap::new(),
+            &DashMap::new(),
             |_req| async move {
                 panic!("Not called");
             },
