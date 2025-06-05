56ef1875f3444bd646ccf95dc1299437e9f7084e
Claudio Bley <claudio.bley@tweag.io>
2025-06-05 16:12:32 +0200

    Add docker build image to flake
    To build and push it the Github packages, run:
    
    ```
    nix run .#dockerBuild \
      | gzip --no-name \
      | nix run nixpkgs#skopeo -- copy \
          --digestfile image.digest \
          --preserve-digests \
          docker-archive:/dev/stdin \
          docker://ghcr.io/owner/repo:tag
    ```
    
    See, e.g.
    
    https://github.com/avdv/buck2/pkgs/container/buck2-build-nix

--- flake.nix
+++ flake.nix
@@ -24,12 +24,94 @@
         overlays = [ (import rust-overlay) ];
       };
 
+      buck2BuildInputs = [
+        pkgs.bashNonInteractive
+        pkgs.cacert
+        pkgs.clang_16
+        pkgs.coreutils
+        pkgs.curl
+        pkgs.git
+        pkgs.gnupg
+        pkgs.gnused
+        pkgs.gnutar
+        pkgs.gzip
+        pkgs.lld_16
+        pkgs.mold-wrapped
+        pkgs.python3
+        pkgs.unzip
+        pkgs.wget
+      ] ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
+        pkgs.stdenv.cc.bintools
+        pkgs.darwin.cctools
+      ];
+
     rust-version = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain;
     my-rust-bin = rust-version.override {
       extensions = [ "rust-analyzer" "rust-src" ];
     };
 
     in {
+      apps.dockerBuild =
+        { program = "${self.packages.${system}.dockerImage}"; type = "app"; };
+
+      packages = {
+        # protoc is downloaded and run during the build but it is dynamically linked and
+        # does not work on NixOS; make it work by using nix-ld
+        baseImage = pkgs.dockerTools.buildImage {
+            name = "base-ld";
+            tag = "latest";
+
+            config = {
+              Env = [
+                # ensure libstdc++.so is found
+                "NIX_LD_LIBRARY_PATH=${pkgs.stdenv.cc.cc.lib}/lib"
+                "NIX_LD=/share/nix-ld/lib/ld.so"
+              ];
+              Label = [
+                # connect github repo to Github package
+                #"org.opencontainers.image.source=https://github.com/owner/repo"
+              ];
+            };
+
+            copyToRoot = pkgs.buildEnv {
+                name = "ld-library-path";
+                pathsToLink = [ "/lib" ];
+                paths = map pkgs.lib.getLib [];
+                postBuild = ''
+                  ln -s ${pkgs.stdenv.cc.bintools.dynamicLinker} $out/share/nix-ld/lib/ld.so
+                '';
+                extraPrefix = "/share/nix-ld";
+                ignoreCollisions = true;
+            };
+
+            runAsRoot = ''
+              #!${pkgs.runtimeShell}
+              mkdir -p /lib64
+              ln -s ${pkgs.nix-ld}/bin/nix-ld /lib64/ld-linux-x86-64.so.2
+            '';
+        };
+
+        dockerImage =
+          let
+            inherit (pkgs) dockerTools python3;
+
+            image = dockerTools.streamNixShellImage {
+              name = "nix-build";
+              drv = pkgs.mkShell.override { stdenv = pkgs.stdenvNoCC; }
+                {
+                    PATH = pkgs.lib.makeBinPath (buck2BuildInputs ++ [my-rust-bin]);
+
+                    nativeBuildInputs = [ python3 ];
+                };
+              shell = pkgs.lib.getExe pkgs.bashNonInteractive;
+              tag = "latest";
+            };
+          in
+            image.override (
+              pkgs.lib.optionalAttrs pkgs.stdenv.isLinux { fromImage = self.packages.${system}.baseImage; }
+            );
+      };
+
       devShells.default = pkgs.mkShell {
         buildInputs = pkgs.lib.optionals pkgs.stdenv.isDarwin (with pkgs.darwin.apple_sdk.frameworks; [
           CoreFoundation
